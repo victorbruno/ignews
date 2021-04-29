3ce10f4d34e731a598c2a95c20dc523257e2ebcc
victorprnt <victorprnt@gmail.com>
2021-04-28 22:43:38 -0300

    Add Home page content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,15 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					{/* <link rel="preconnect" .../> */}
-					{/* <link href="https://fonts.google..." ... /> */}
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap" rel="stylesheet"/>
 				</Head>
 				<body>
 					<Main />
 					<NextScript />
 				</body>
 			</Html>
-		)
+		);
 	}
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Home</title>
+      <title>Home | ig.news</title>
     </Head>
 
     <Header />
-    
+
+    <main className={styles.homeContainer}>
+      <section className={styles.homeContent}>
+        <span>Hey, welcome</span>
+        <h1>
+          News about <br/>
+          the <span>React</span> world
+        </h1>
+        <p>
+          Get access to all the publications <br/>
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt=""/>
+    </main>
+
     </>
   );
 }
