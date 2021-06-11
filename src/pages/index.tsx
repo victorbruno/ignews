722710ada3d0d738fe11f72259546bb4ea802310
victorprnt <victorprnt@gmail.com>
2021-06-10 23:31:24 -0300

    Feat[Home]: Get API data from Stripe
    Subsciption on Home is now getting data from the product registered at Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,62 @@
-import Head from 'next/head'
-import { Header } from '../components/Header'
-import { SubscribeButton } from '../components/SubscribeButton';
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { Header } from "../components/Header";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
-    <Head>
-      <title>Home | ig.news</title>
-    </Head>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
 
-    <Header />
+      <Header />
 
-    <main className={styles.homeContainer}>
-      <section className={styles.homeContent}>
-        <span>Hey, welcome</span>
-        <h1>
-          News about <br/>
-          the <span>React</span> world
-        </h1>
-        <p>
-          Get access to all the publications <br/>
-          <span>for $9.90 month</span>
-        </p>
-        <SubscribeButton />
-      </section>
-      <img src="/images/avatar.svg" alt=""/>
-    </main>
-
+      <main className={styles.homeContainer}>
+        <section className={styles.homeContent}>
+          <span>Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="" />
+      </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1J0zjIB70pYtdXBlftvKsoPl", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
